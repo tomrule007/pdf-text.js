dc1e12d163b34dddf65c8120276ee99efc71f3e0
Thomas Herzog <Tomrule007@gmail.com>
2020-01-30 17:01:40 -0800

    Add PDF component from pdf.js/example/ CRA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import pdfjs from 'pdfjs-dist';
-import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+// import pdfjs from 'pdfjs-dist';
+// import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 
 import FileInput from './FileInput';
+import PDF from './components/PDF';
 
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 function App() {
   const [invoiceFolder, setInvoiceFolder] = useState([]);
   console.log('something');
@@ -15,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <FileInput onChange={handleFileInputChange}></FileInput>
+      <PDF />
       <ul>
         {invoiceFolder.map(invoice => (
           <li>{invoice.name}</li>
