4c8325a7ff68e800c10d5ace04c7095c93f7be2d
Thomas Herzog <Tomrule007@gmail.com>
2020-02-04 00:27:40 -0800

    pdfText util now returns array of text rows.
    Hacky implimentation as proof of concept.

--- src/utilities/pdfText.js
+++ src/utilities/pdfText.js
@@ -14,8 +14,28 @@ const pdfText = async src => {
   for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     const pageTextContent = await page.getTextContent();
-    const pageJustTextArray = pageTextContent.items.map(item => item.str);
-    textContent.push(...pageJustTextArray);
+    // const pageJustTextArray = pageTextContent.items.map(item => item.str);
+    let pageTextArray = [];
+    let currentRow = [];
+    let lastYcord = null;
+    for (let j = 0; j < pageTextContent.items.length; j++) {
+      const item = pageTextContent.items[j];
+      const allowedVariance = 1;
+      const y = item.transform[4];
+      if (
+        y === null ||
+        y === lastYcord ||
+        (y < lastYcord && y > lastYcord - allowedVariance) ||
+        (y > lastYcord && y < lastYcord + allowedVariance)
+      ) {
+        currentRow.push(item.str);
+      } else {
+        pageTextArray.push([...currentRow]);
+        currentRow = [item.str];
+      }
+      lastYcord = y;
+    }
+    textContent.push(...pageTextArray);
   }
 
   return textContent;
