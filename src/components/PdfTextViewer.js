3dc45c8bf112cc76de43788ecb80f8f7fef8964c
Thomas Herzog <Tomrule007@gmail.com>
2020-02-04 00:41:44 -0800

    Changed PdfTextViewer to render html table.

--- src/components/PdfTextViewer.js
+++ src/components/PdfTextViewer.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import pdfText from '../utilities/pdfText';
 export default function PdfTextViewer({ file }) {
-  const [fileText, setFileText] = useState(['loading...']);
+  const [fileText, setFileText] = useState([['loading...']]);
   useEffect(() => {
     const fetchPdfText = async () => {
       const data = await file.arrayBuffer();
@@ -16,11 +16,17 @@ export default function PdfTextViewer({ file }) {
   return (
     <div>
       <h2>{file.name}</h2>
-      <ul>
-        {fileText.map(text => (
-          <li>{text}</li>
+      <table
+        style={{ border: '1px solid black', 'border-collapse': 'collapse' }}
+      >
+        {fileText.map(row => (
+          <tr style={{ border: '1px solid black' }}>
+            {row.map(cell => (
+              <td style={{ border: '1px solid black' }}>{cell}</td>
+            ))}
+          </tr>
         ))}
-      </ul>
+      </table>
     </div>
   );
 }
