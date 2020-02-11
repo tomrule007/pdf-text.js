b4dfacb09c4557c5b441f38ac52cb12f019793de
Thomas Herzog <Tomrule007@gmail.com>
2020-02-11 00:14:29 -0800

    Removed unneeded character object properties

--- src/utilities/fillTextIntercept.js
+++ src/utilities/fillTextIntercept.js
@@ -1,7 +1,6 @@
 /**
  * fillTextIntercept modified a canvas context object to intercept all calls to 'fillText' and recorder results into an array
  * Inspired by: https://www.garysieling.com/blog/extracting-tables-from-pdfs-in-javascript-with-pdf-js
- * @author: Thomas Herzog
  * @param {CanvasRenderingContext2D} ctx canvas context to intercept calls on.
  * @param {Boolean} debug Set to true to draw boxes around each 'fillText' call
  */
@@ -11,23 +10,14 @@ export default function fillTextIntercept(ctx, debug = false) {
   var fillText = ctx.fillText;
 
   ctx.fillText = function(text, x, y) {
-    const code = text.charCodeAt(0);
-
-    // Unknown character encoding issue that is solved with this offset (for my specific pdf)
-    const CHAR_CODE_OFFSET = 57344; //sales invoice
-    const realCode = code - CHAR_CODE_OFFSET;
-    const realChar = String.fromCharCode(realCode);
     const width = ctx.measureText(text).width;
 
     // Store Character info in chars array
     chars[chars.length] = {
       text,
-      realChar,
       x: ctx._transformMatrix[4] + x,
       y: ctx._transformMatrix[5] + y,
-      width,
-      code,
-      realCode
+      width
     };
 
     if (debug) {
