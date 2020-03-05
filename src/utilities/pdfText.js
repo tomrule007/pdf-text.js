a08c67c4caa97684d1311a51eb2ed29efc201d18
Thomas Herzog <Tomrule007@gmail.com>
2020-03-05 14:08:08 -0800

    Merge fillTextIntercept into pdfText file

--- src/utilities/pdfText.js
+++ src/utilities/pdfText.js
@@ -1,9 +1,61 @@
 import pdfjs from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 
-import fillTextIntercept from './fillTextIntercept';
-
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+const getRealXY = (transformMatrix, [x, y]) => {
+  const { a, b, c, d, e, f } = transformMatrix;
+  // Apply the transformation to input coordinates using matrix dot product formula
+  //
+  // | a c e |   | x |    |x * a + y * c + 1 * e |
+  // | b d f | * | y |  = |x * b + y * d + 1 * f |
+  // | 0 0 1 |   | 1 |    |x * 0 + y * 0 + 1 * 1 |
+
+  return [x * a + y * c + 1 * e, x * b + y * d + 1 * f];
+};
+
+/**
+ * fillTextIntercept modified a canvas context object to intercept all calls to 'fillText' and recorder results into an array
+ * Inspired by: https://www.garysieling.com/blog/extracting-tables-from-pdfs-in-javascript-with-pdf-js
+ * @param {CanvasRenderingContext2D} ctx canvas context to intercept calls on.
+ * @param {Object} storageObject store all found characters in the 'chars' property on this object.
+ */
+function fillTextIntercept(ctx, storageObject) {
+  const CHAR_CODE_OFFSET = 0;
+  // Add chars array to storageObject
+  if (storageObject.chars)
+    throw new Error(
+      `fillTextIntercept attempted to override '.chars' on storageObject: ${storageObject}`
+    );
+  // eslint-disable-next-line no-param-reassign
+  storageObject.chars = [];
+
+  // Save reference to the real fillText function
+  const { fillText } = ctx;
+
+  ctx.fillText = function intercept(text, x, y) {
+    const realChar = String.fromCharCode(text.charCodeAt(0) - CHAR_CODE_OFFSET);
+    const transformMatrix = ctx.getTransform();
+    const [realX, realY] = getRealXY(transformMatrix, [x, y]);
+    const width = ctx.measureText(realChar).width * transformMatrix.a;
+
+    // Store Character info in chars array
+    /* 
+      Probably should be using some type of callback storage solution.
+      For now I am just adding a storageProperty to the page object
+    */
+    // eslint-disable-next-line no-param-reassign
+    storageObject.chars[storageObject.chars.length] = {
+      text: realChar,
+      x: realX,
+      y: realY,
+      width
+    };
+
+    // Call real filltext function
+    fillText.apply(ctx, [text, x, y]);
+  };
+}
 
 const addCanvasAndRender = page => {
   const scale = 1.5;
