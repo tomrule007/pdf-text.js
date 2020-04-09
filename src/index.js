dec6611424187d9511597dccc92d2ae4d804c657
Thomas Herzog <Tomrule007@gmail.com>
2020-04-09 13:19:14 -0700

    Wire redux-persist to rootReducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import store from './store';
+import { store, persistor } from './store';
 import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './rootReducer';
 
-const store = configureStore({
-  reducer: rootReducer
+const persistConfig = {
+  key: 'root',
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer
 });
+export const persistor = persistStore(store);
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept('./rootReducer', () => {
     const newRootReducer = require('./rootReducer').default;
-    store.replaceReducer(newRootReducer);
+    store.replaceReducer(persistReducer(persistConfig, newRootReducer));
   });
 }
-
-export default store;
