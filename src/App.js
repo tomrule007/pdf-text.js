bcc543c2dc7706b4c1c9cc00b6d8542a796b6693
Thomas Herzog <Tomrule007@gmail.com>
2020-09-10 08:48:24 -0700

    WIP: Setting up demo page

--- src/App.js
+++ src/App.js
@@ -1,89 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import localForage from 'localforage';
+import React, { useState } from 'react';
 
-import FileInput from './components/FileInput';
 import PdfData from './components/PdfData';
 
 import defaultTemplateFile from './sampleFiles/sampleTables.json';
 import defaultPdfFile from './sampleFiles/sampleTables.pdf';
 
-import TemplateCreator from './components/TemplateCreator';
-import MyDropzone from './components/MyDropzone/MyDropzone';
-
-import { loadFiles } from './feature/file/fileSlice';
-
 function App() {
-  const dispatch = useDispatch();
-  const files = useSelector(state => state.file.files);
-  const [templateFile, setTemplateFile] = useState(defaultTemplateFile);
-  const [pdfFile, setPdfFile] = useState(defaultPdfFile);
+  const [templateFile, setTemplateFile] = useState(undefined);
+  const [pdfFile, setPdfFile] = useState(undefined);
 
-  useEffect(() => {
-    const fileRef = files[files.length - 1];
-    if (fileRef) {
-      switch (fileRef.type) {
-        case 'application/pdf':
-          localForage
-            .getItem(fileRef.key)
-            .then(file => file.arrayBuffer())
-            .then(setPdfFile);
-          break;
-        case 'application/json':
-          localForage
-            .getItem(fileRef.key)
-            .then(file => file.text())
-            .then(JSON.parse)
-            .then(setTemplateFile);
-          break;
-        default:
-          break;
-      }
-    }
-  }, [files]);
-  const handlePdfFileChange = e => dispatch(loadFiles(e.target.files));
-  const handleTemplateFileChange = e => dispatch(loadFiles(e.target.files));
+  const handlePdfFileChange = (e) => {
+    console.log('pdf file', e.target.files[0].arrayBuffer().then(setPdfFile));
+  };
+  const handleTemplateFileChange = (e) => {
+    console.log(
+      'template file',
+      e.target.files[0].text().then(setTemplateFile)
+    );
+  };
+
+  const fileBoxStyle = {
+    border: '2px solid black',
+    borderRadius: '5px',
+    boxSizing: 'border-box',
+    margin: '10px',
+  };
   return (
     <div className="App">
-      <FileInput
-        labelText="Select a pdf file:"
-        accept=".pdf"
-        onChange={handlePdfFileChange}
-        htmlFor="pdfInput"
-      />
-      <FileInput
-        labelText="Select a template file:"
-        accept=".json"
-        onChange={handleTemplateFileChange}
-        htmlFor="templateInput"
-      />
-      <span>
-        {'Download: '}
-        <a
-          href={`${process.env.PUBLIC_URL}/pdfs/sampleTables.pdf`}
-          download="sampleTable.pdf"
-        >
-          sampleTable.pdf
-        </a>
-        {' / '}
-        <a
-          href={`${process.env.PUBLIC_URL}/templates/sampleTables.json`}
-          download="sampleTable.json"
-        >
-          sampleTable.json
-        </a>
-        {' / '}
-        <a
-          href={`${process.env.PUBLIC_URL}/templates/invoice.json`}
-          download="invoice.json"
-        >
-          invoice.json
-        </a>
-      </span>
-
-      <MyDropzone />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          textAlign: 'center',
+          marginTop: '10px',
+        }}
+      >
+        <div>
+          <a
+            style={{ padding: '20px' }}
+            href={`${process.env.PUBLIC_URL}/pdfs/sampleTables.pdf`}
+            title="demo pdf file"
+            download="sampleTable.pdf"
+          >
+            sampleTable.pdf
+          </a>
+          <div style={fileBoxStyle}>
+            <h3>PDF File</h3>
+            <input type="file" onChange={handlePdfFileChange} accept=".pdf" />
+          </div>
+        </div>
+        <div>
+          <h2>
+            PDF Template Parse <br /> Demo
+          </h2>
+        </div>
+        <div>
+          <a
+            href={`${process.env.PUBLIC_URL}/templates/sampleTables.json`}
+            title="demo template file"
+            download="sampleTable.json"
+          >
+            sampleTable.json
+          </a>
+          <div style={fileBoxStyle}>
+            <h3>Template File</h3>
+            <input
+              type="file"
+              onChange={handleTemplateFileChange}
+              accept=".json"
+            />
+          </div>
+        </div>
+      </div>
       <PdfData pdf={pdfFile} template={templateFile} />
-      <TemplateCreator file={pdfFile} />
     </div>
   );
 }
