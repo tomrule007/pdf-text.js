857b2fa4a5a1bdf760ba27a6d9714eadc29e85b4
Thomas Herzog <Tomrule007@gmail.com>
2020-09-10 16:06:03 -0700

    Remove local copy of pdf-template-parse

--- packages/pdf-template-parse/index.js
+++
@@ -1,7 +0,0 @@
-import getPdfChars from './pdfChars';
-import pdfText from './pdfText';
-
-const pdfTemplateParse = pdfText;
-const pdfParse = getPdfChars;
-
-export { pdfTemplateParse as default, pdfParse };
